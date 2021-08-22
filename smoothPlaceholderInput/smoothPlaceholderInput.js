67c643a5b8b5d9941b4ca7a7896361a91cf73ca4
zphyrj <46908127+zphyrj@users.noreply.github.com>
2021-08-21 18:50:54 -0700

    organized js into src folder

--- smoothPlaceholderInput/smoothPlaceholderInput.js
+++ smoothPlaceholderInput/smoothPlaceholderInput.js
@@ -2,51 +2,61 @@
 class smoothPlaceholderInput extends HTMLElement {
   constructor() {
     super();
-    this.attachShadow({mode: 'open'});
     this.input = document.createElement("input");
     this.placeholder = document.createElement("label");
     this.placeholder.className = "placeholder";
+    this.container = document.createElement("div");
+    this.container.className = "container";
     this.styleElement = document.createElement("style");
     this.styleElement.innerHTML = `
       .placeholder {
         position: absolute;
-        top: 0;
+        top: .5em;
         left: 0;
         color: #aaa;
-        font-size: 1.2em;
+        font-size: 1em;
         pointer-events: none;
         transition: all 0.2s ease-in-out;
+        font-family: inherit;
+        font-weight: inherit;
       }
       input:focus ~ .placeholder {
-        top: -.2em;
-        font-size: 1em;
-        color: #000;
+        left: 0;
+        transform: translate3d(0,-.83em,0) scale(.83);
+        transform-origin: left center;
+        color: #777;
       }
-      :host {
+      input::placeholder {
+        color: transparent;
+      }
+      .container {
         display: block;
         position: relative;
         width: 100%;
-        height: 2em;
-        border: 1px solid #ccc;
-        border-radius: 3px;
-        padding: 0.5em;
-        box-sizing: border-box;
+        height: 3em;
+        padding: 0;
+        margin: 0;
+      }
+      .host {
+        display: block;
+        position: relative;
       }
       input { 
         position: absolute;
-        top: 0;
+        bottom: -.2em;
         left: 0;
         width: 100%;
         height: 100%;
-        border: none;
+        box-sizing: border-box;
         outline: none;
+        border: none;
         background: none;
         font-size: 1em;
+        font-family: inherit;
         color: #000;
         transition: all 0.2s ease-in-out;
       }
     `;
-    this.shadowRoot.appendChild(this.styleElement);
   }
   connectedCallback() {
     console.log('HERE');
@@ -57,8 +67,47 @@ class smoothPlaceholderInput extends HTMLElement {
       }
       this.input.setAttribute(attr, this.getAttribute(attr));
     });
-    this.shadowRoot.appendChild(this.input);
-    this.shadowRoot.appendChild(this.placeholder);
+    this.appendChild(this.container);
+    this.container.appendChild(this.styleElement);
+    this.container.appendChild(this.input);
+    this.container.appendChild(this.placeholder);
+    this.classList.add("host");
+    this.placeholder.for = this.input.id;
+    let frozen = false;
+    const inputChangeHandler = ()=>
+    {
+      if (this.input.value.length > 0)
+      {
+        if (!frozen) {
+          this.styleElement.sheet.insertRule(`
+          .placeholder {
+            left: 0 !important;
+            transform: translate3d(0,-.83em,0) scale(.83) !important;
+            transform-origin: left center !important;
+            color: #777 !important;
+          }
+          `, 0);
+          frozen = true;
+        }
+      }
+      else
+      {
+        if (frozen) {
+          this.styleElement.sheet.removeRule(0);
+          frozen = false;
+        }
+      }
+    }
+    this.input.addEventListener("keydown", inputChangeHandler);
+    this.input.addEventListener("keyup", inputChangeHandler);
+    this.input.addEventListener("change", inputChangeHandler);
+    this.input.addEventListener("input", inputChangeHandler);
+    this.input.addEventListener("focus", ()=>{
+      this.classList.add("focus");
+    });
+    this.input.addEventListener("blur", ()=>{
+      this.classList.remove("focus");
+    });
   }
 }
 customElements.define('smooth-placeholder-input', smoothPlaceholderInput);
